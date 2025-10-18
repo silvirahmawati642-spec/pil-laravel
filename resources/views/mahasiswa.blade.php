eb0494e6ca8ba92cb95a81d6cda70baf5027b63b
unknown <silvirahmawati642-spec>
2025-10-18 22:57:11 +0700

    Belajar mengenai tabel

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -35,8 +35,52 @@
   </div>
 </nav>
 
-<div class="container" style="margin-left: 30px">
+<div class="container">
   <h1>Ini adalah halaman Mahasiswa</h1>
+
+  {{--
+    ROWSPAN = untuk menggabungkan baris
+    COLSPAN = untuk menggabungkan kolom
+    --}}
+
+  <table class="table table-danger table-sm table-hover table-striped table-bordered 
+  text-center">
+    <thead>
+      <tr>
+        <th>NPM</th>
+        <th>Nama Mahasiswa</th>
+        <th>Jenis Kelamin</th>
+        <th colspan="2">TTL</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>0702232100</td>
+        <td>Silvi Rahmawati</td>
+        <td>Perempuan</td>
+        <td>14-07-2005</td>
+        <td rowspan="4">Tegal</td>
+      </tr>
+      <tr>
+        <td>0702232101</td>
+        <td>Fira Alifia Putri</td>
+        <td>Perempuan</td>
+        <td>16-07-2017</td>
+      </tr>
+      <tr>
+        <td>0702232102</td>
+        <td>Uswatun Hasanah</td>
+        <td>Perempuan</td>
+        <td>15-07-2020</td>
+      </tr>
+      <tr>
+        <td>0702232103</td>
+        <td>Rasheed Al Ghifari Akbar</td>
+        <td>Laki-Laki</td>
+        <td>05-08-2025</td>
+      </tr>
+    </tbody>
+  <table>
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
