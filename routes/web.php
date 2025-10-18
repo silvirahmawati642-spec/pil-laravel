9f287e8789cc3272050bd3a772c9a3fcc0778fc1
unknown <silvirahmawati642-spec>
2025-10-18 23:18:41 +0700

    Belajar FOR

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,7 @@ Route::get('profile', function () {
 }); 
 
 Route::get('array',function(){
-    // Infinite Loop
-    $nilai_awal = 1;
-    while ($nilai_awal <= 100) {
-        echo 'Hello Laravel ' . $nilai_awal . 'x<br>';
-        $nilai_awal++;
+    for ($i=1; $i <= 5; $i++) {
+        echo 'Hello World ' . $i .'x<br>';
     }
 });
